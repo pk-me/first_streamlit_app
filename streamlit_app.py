f8d2897e0a124186b360b37183df93f6ce93fb66
pk-me <87186071+pk-me@users.noreply.github.com>
2023-08-08 13:33:21 +0200

    Update streamlit_app.py
    read file from bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title("My parents new healthy Diner")
 st.header('Breakfast Menu')
-st.text('🥣Omega 3 & Blueberry Oatmeal')
-st.text(' 🥗Kale, Spinach & Rocket Smoothie')
-st.text('🐔Hard-Boiled Free-Range Egg')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
