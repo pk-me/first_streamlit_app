805456d03f77060f2a74fc68a6622029eb2c22da
pk-me <87186071+pk-me@users.noreply.github.com>
2023-08-08 11:32:33 +0200

    Update streamlit_app.py
    breakfast menu 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("My parents new healthy Diner")
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
