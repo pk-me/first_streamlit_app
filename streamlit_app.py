ba3de9aab5b7a99ef94959cc45b93c9efcd79407
pk-me <87186071+pk-me@users.noreply.github.com>
2023-08-09 11:38:38 +0200

    Update streamlit_app.py
    upd request

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ st.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
